70483088d555d336a44338a4b8ef68515dc91564
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-08-13 23:31:19 +0800

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD68L0Gfqp9Br1kVqkMJd85BuTxwqIRVlA",
+	authDomain: "netflix-clone-react-4e686.firebaseapp.com",
+	projectId: "netflix-clone-react-4e686",
+	storageBucket: "netflix-clone-react-4e686.appspot.com",
+	messagingSenderId: "671259807800",
+	appId: "1:671259807800:web:eff49634dd88a8190ed6cd",
+};
