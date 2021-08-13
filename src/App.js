798db50f502f7ec43835de4b96365b5c7f6d6cb6
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-08-14 00:09:22 +0800

    home screen setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
 
 function App() {
 	return (
 		<div className="app">
-			<h1>NetFlix Clone React</h1>
+			<HomeScreen />
 		</div>
 	);
 }
---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+	return (
+		<div className="homeScreen">
+			<h1>HomeScreen Component</h1>
+		</div>
+	);
+}
+
+export default HomeScreen;
