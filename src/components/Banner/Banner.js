d0a1c22f092f61bb8055be05348cccea3927de93
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-08-15 00:21:32 +0800

    fetched the movies data from the tmdb

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import "./Banner.css";
 
+import requests from "../../api/Requests";
+import axios from "../../api/axios_request";
+
 function Banner() {
+	const [movie, setMovie] = useState([]);
+
+	useEffect(() => {
+		const fetchMovieData = async () => {
+			const request = await axios.get(requests.fetchNetflixOriginals);
+			setMovie(
+				request.data.results[
+					Math.floor(Math.random() * request.data.results.length - 1)
+				]
+			);
+
+			return requests;
+		};
+
+		fetchMovieData();
+	}, []);
+
+	console.log(movie);
+
 	const truncateDescription = (string, n) => {
 		return string?.length > n ? string.substr(0, n - 1) + "..." : string;
 	};
@@ -10,23 +33,21 @@ function Banner() {
 		<header
 			className="banner"
 			style={{
-				backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
 				backgroundSize: "cover",
 				backgroundPosition: "center center",
 			}}
 		>
 			<div className="banner__contents">
-				<h1 className="banner__title">Movie Name</h1>
+				<h1 className="banner__title">
+					{movie?.title || movie?.name || movie?.original_name}
+				</h1>
 				<div className="banner__buttons">
 					<button className="banner__button">Play</button>
 					<button className="banner__button">My List</button>
 				</div>
 				<h1 className="banner__description">
-					{truncateDescription(
-						`This is a test description This is a test description This is a test
-					description This is a test description This is a test description This is a test description This is a test description This is a test description`,
-						150
-					)}
+					{truncateDescription(`${movie?.overview}`, 150)}
 				</h1>
 			</div>
 			<div className="banner--fadeBottom" />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -25,12 +25,12 @@ function Navbar() {
 				<img
 					className="nav__logo"
 					src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-					alt=""
+					alt="netlif-logo"
 				/>
 				<img
 					className="nav__avatar"
 					src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-					alt=""
+					alt="avatar"
 				/>
 			</div>
 		</div>
